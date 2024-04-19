a727375348e1b5df47e1dc5f9ffcacc64d66e0fb
Perry <perry.mcghee@outlook.com>
2024-04-19 17:52:50 +0100

    Initial commit

---
+++ LifePlanner/LifePlanner/Data/MainDBContext.cs
@@ -0,0 +1,17 @@
+﻿using LifePlanner.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace LifePlanner.Data
+{
+    public class MainDBContext :DbContext
+    {
+
+        public MainDBContext(DbContextOptions<MainDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ LifePlanner/LifePlanner/Program.cs
@@ -0,0 +1,42 @@
+using LifePlanner.Client.Pages;
+using LifePlanner.Components;
+using LifePlanner.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Default")
+    ?? throw new NullReferenceException("No connection string in config");
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+    
+builder.Services.AddDbContextFactory<MainDBContext>((DbContextOptionsBuilder options) => 
+    options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(LifePlanner.Client._Imports).Assembly);
+
+app.Run();
